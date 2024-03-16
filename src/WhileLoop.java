c4529d9fe9ad26853fed87ec6a8220ec8ee3b1d5
LOKESH <lokesh.r@gamucopia.com>
2024-03-16 08:50:21 +0530

    solved some

---
+++ src/WhileLoop.java
@@ -0,0 +1,13 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        final int capacity = 30;
+        int carsparked = 0;
+        while (carsparked <= capacity) {
+            System.out.println("car" + " " + carsparked + " " + "is parked");
+//            fixme -Print the number of cars being parked
+//            TODO-
+            carsparked++;
+        }
+    }
+}
+
