d8a2e05d5ea8c9d4881e5a93b858a4e86f1129d1
karthick <karthickmohan419@gmail.com>
2024-03-16 08:47:54 +0530

    karthi

---
+++ src/java.java
@@ -0,0 +1,11 @@
+public class java {
+    public static void main(String[] args) {
+        for (int i= 1; i <=100 ; i++){
+            sum = i + sum
+            System.out.println(i);
+        }
+    }
+}
+
+
+
