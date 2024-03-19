93d3b88c4e77454f1ef4504fc113f16f07b9ca2b
LOKESH <lokesh.r@gamucopia.com>
2024-03-19 22:19:22 +0530

    solved

---
+++ src/LOKESH/EvenOrOdd.java
@@ -0,0 +1,12 @@
+package LOKESH;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int number = 777;
+        if (number % 2 == 0)
+            System.out.println(number+" "+ "even");
+        else
+            System.out.println(number +" "+ "odd");
+
+    }
+}
