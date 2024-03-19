6369bfe33dada99ca3e7fcee2bd1822823fea66b
karthick <karthickmohan419@gmail.com>
2024-03-19 08:15:37 +0530

    karthi

---
+++ src/Numbers.java
@@ -0,0 +1,8 @@
+public class Numbers {
+    public static void main(String[] args) {
+        for (int karthi = 1; karthi <=10;  karthi++){
+        System.out.println("print all numbers"+karthi);
+        }
+    }
+}
+
