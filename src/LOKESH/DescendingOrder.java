7715194f3c267b8544c22ccfa93bbd27faf4bcab
LOKESH <lokesh.r@gamucopia.com>
2024-03-19 21:54:12 +0530

    solved

---
+++ src/LOKESH/DescendingOrder.java
@@ -0,0 +1,20 @@
+package LOKESH;
+
+public class DescendingOrder {
+    public static void main(String[] args) {
+        int[] arr = new int[]{889, 96, 333, 417, 500};
+        int des = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    des = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = des;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
