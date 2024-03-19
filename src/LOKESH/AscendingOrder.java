a9c671e490bfe2d80ace1fee320788ee07153a6e
LOKESH <lokesh.r@gamucopia.com>
2024-03-19 20:58:20 +0530

    solved

---
+++ src/LOKESH/AscendingOrder.java
@@ -0,0 +1,22 @@
+package LOKESH;
+
+import java.util.Arrays;
+
+public class AscendingOrder {
+    public static void main(String[] args) {
+        int[] arr = new int[]{88, 96, 333, 417, 500};
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
