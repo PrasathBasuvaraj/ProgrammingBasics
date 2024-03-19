22dae89e57c8fa3730ba2bdbe6ed01ecad7844fe
LOKESH <lokesh.r@gamucopia.com>
2024-03-19 22:49:44 +0530

    solved

---
+++ src/LOKESH/PalindromeString.java
@@ -0,0 +1,21 @@
+package LOKESH;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        String name = "kayak";
+        boolean flag = true;
+        name = name.toLowerCase();
+        for (int i = 0; i < name.length() / 2; i++) {
+            if (name.charAt(i) != name.charAt(name
+                    .length() - i - 1)) {
+                flag = false;
+                break;
+            }
+        }
+        if (!flag)
+            System.out.println(name + " " + "not palidrome");
+        else
+            System.out.println(name + " " + "is palindrome ");
+
+    }
+}
