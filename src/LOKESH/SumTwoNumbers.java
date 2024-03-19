a82c84ea83ae0bd14c6b74a520695de1d88dd6a7
Prasath Basuvaraj <prasath.buddie@gmail.com>
2024-03-18 22:06:15 -0400

    formatted

--- src/LOKESH/SumTwoNumbers.java
+++ src/LOKESH/SumTwoNumbers.java
@@ -2,8 +2,8 @@ package LOKESH;
 
 public class SumTwoNumbers {
     public static void main(String[] args) {
-        int Sum1=20;
-        int Sum2=30;
-        System.out.println(Sum1+Sum2);
+        int Sum1 = 20;
+        int Sum2 = 30;
+        System.out.println(Sum1 + Sum2);
     }
 }
