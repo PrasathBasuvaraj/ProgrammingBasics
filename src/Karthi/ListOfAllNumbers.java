edaa9851ded1f269fdcec93b35fe7d3c90281338
karthick <karthickmohan419@gmail.com>
2024-03-20 22:15:15 +0530

    karthi

---
+++ src/Karthi/ListOfAllNumbers.java
@@ -0,0 +1,9 @@
+package Karthi;
+
+public class ListOfAllNumbers {
+    public static void main(String[] args) {
+        for (int counter = 0; counter < 10 ; counter++) {
+            System.out.println(counter);
+        }
+    }
+}
